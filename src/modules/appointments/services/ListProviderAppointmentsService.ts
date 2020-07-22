ccae37ab051fd5f6a20f56de67a135d210654117
Lucas Faria <lucashpharia@gmail.com>
2020-07-22 12:03:47 -0300

    cache serialization

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 
 import { injectable, inject } from 'tsyringe';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 import Appointment from '../infra/typeorm/entities/Appointment';
 
@@ -45,7 +46,7 @@ class ListProviderAppointmentService {
       );
     }
 
-    await this.cacheProvider.save(cacheKey, appointments);
+    await this.cacheProvider.save(cacheKey, classToClass(appointments));
 
     return appointments;
   }
