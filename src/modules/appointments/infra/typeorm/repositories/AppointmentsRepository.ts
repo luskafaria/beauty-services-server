6bdb861a2bd40bf526b18b43ca34126856b44f8e
Lucas Faria <lucashpharia@gmail.com>
2020-07-22 11:52:10 -0300

    provider appointments eager user data

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -39,6 +39,7 @@ class AppointmentsRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
